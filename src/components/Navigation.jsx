2d8ea3572d76c9fbe510a4ded8f6a7a7abef22bf
YVeselovskyi <19dynamokyiv94@gmail.com>
2019-06-25 16:34:45 +0300

    Styling added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,30 +3,37 @@ import { Link } from 'react-router-dom';
 
 import { routes } from 'config';
 
+import Logo from 'assets/images/logo.svg';
+
 export const Navigation = () => (
-  <ul>
-    <li>
-      <Link to={routes.home}>Home</Link>
-    </li>
-
-    <li>
-      <Link to={routes.cars}>cars</Link>
-    </li>
-
-    <li>
-      <Link to={routes.car.red}>red car</Link>
-    </li>
-
-    <li>
-      <Link to={routes.car.green}>green car</Link>
-    </li>
-
-    <li>
-      <Link to="/car/pink">another car</Link>
-    </li>
-
-    <li>
-      <Link to="/notfoundpath">Not found</Link>
-    </li>
-  </ul>
+  <div className="header-wrapper">
+    <header className="header-item">
+      <Link to={routes.home} className="react-link">
+        <img className="logo" src={Logo} alt="React" />
+        <h2>React</h2>
+      </Link>
+      <nav>
+        <ul className="site-navigation">
+          <li>
+            <Link to={routes.home}>Home</Link>
+          </li>
+          <li>
+            <Link to={routes.cars}>Cars</Link>
+          </li>
+          <li>
+            <Link to={routes.car.red}>Red car</Link>
+          </li>
+          <li>
+            <Link to={routes.car.green}>Green car</Link>
+          </li>
+          <li>
+            <Link to="/car/pink">Another car</Link>
+          </li>
+          <li>
+            <Link to="/notfoundpath">Not found</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
 );
