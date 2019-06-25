bac32727049653cff5c8b1ac346f3ca390964e4b
YVeselovskyi <19dynamokyiv94@gmail.com>
2019-06-25 11:00:58 +0300

    App routes fix

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { Home, Cars } from 'pages';
-import { RedCar, GreenCar, OtherCar } from 'pages/Cars';
+import { RedCar, GreenCar, AnotherCar } from 'pages/Cars';
 import { NotFound } from 'components';
 
 import { routes } from 'config';
@@ -13,7 +13,7 @@ export const AppRoutes = () => (
     <Route exact path={routes.cars} component={Cars} />
     <Route exact path={routes.car.red} component={RedCar} />
     <Route exact path={routes.car.green} component={GreenCar} />
-    <Route exact path={routes.otherCar} component={OtherCar} />
+    <Route exact path={routes.otherCar} component={AnotherCar} />
     <Route component={NotFound} />
   </Switch>
 );
